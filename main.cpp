3d91423ba12a8a42fa3cb67fc9eaad011bcf25b6
Andrew DM <andrew.nc.dm@gmail.com>
2021-03-13 21:40:09 +0200

    Complete window

--- main.cpp
+++ main.cpp
@@ -2,17 +2,35 @@
 #define GL_SILENCE_DEPRECATION
 #endif
 
+#define GLFW_INCLUDE_NONE
+
+#include <iostream>
 #include <GLFW/glfw3.h>
+#include <OpenGL/gl3.h>
+
+void error_callback(int error, const char* message)
+{
+    fprintf(stderr, "Error: %s\n", message);
+}
+
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+    }
+}
 
 int main()
 {
-    GLFWwindow* window;
+    glfwSetErrorCallback(error_callback);
 
     if (!glfwInit())
     {
         return -1;
     }
 
+    GLFWwindow* window;
     window = glfwCreateWindow(640, 480, "Hello, World!", NULL, NULL);
 
     if (!window)
@@ -22,6 +40,13 @@ int main()
     }
 
     glfwMakeContextCurrent(window);
+    
+    glfwSetKeyCallback(window, key_callback);
+
+    int width, height;
+    glfwGetFramebufferSize(window, &height, &width);
+    
+    glViewport(0, 0, width, height);
 
     while (!glfwWindowShouldClose(window))
     {
@@ -30,6 +55,7 @@ int main()
         glfwPollEvents();
     }
 
+    glfwDestroyWindow(window);
     glfwTerminate();
 
     return 1;
